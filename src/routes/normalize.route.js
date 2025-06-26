fef928e5e02a27cd7e23dba0b04b220dd51a9880
Railan Santana <lansilvah14fsa@gmail.com>
2025-06-26 01:24:26 -0300

    refactor: melhorar validação e tratamento de erros na rota de normalização

--- src/routes/normalize.route.js
+++ src/routes/normalize.route.js
@@ -1,27 +1,22 @@
-import { Router } from 'express';
-import { runNormalizationPipeline } from '../strategies/orchestrator.js';
+import { Router } from "express";
+import { runNormalizationPipeline } from "../strategies/orchestrator.js";
 
 const router = Router();
 
-router.post('/normalize', async (req, res) => {
-    try {
-        const {provider, rawData} = req.body;
-
-        // console.log("Dados recebidos:", { provider, rawData });
-
-        if(!rawData){
-            return res.status(400).json({ error: "A chave 'rawData' é obrigatória." });
-        }
-        // Delega toda a lógica para o pipeline
-        const result = runNormalizationPipeline(rawData, provider);
-
-        return res.status(200).json(result);
-    } catch (err) {
-         console.error("Erro na rota /normalize:", err);
-        const errorMessage = err instanceof Error ? err.message : String(err);
-        return res.status(500).json({ error: "Erro interno no servidor.", details: errorMessage });
-        
+router.post("/normalize", async (req, res) => {
+  try {
+    const { provider, rawData } = req.body;
+    if (!rawData || !provider) {
+      return res.status(400).json({ error: "As chaves 'provider' e 'rawData' são obrigatórias." });
     }
+    const result = await runNormalizationPipeline(rawData, provider);
+    return res.status(200).json(result);
+
+  } catch (err) {
+    console.error("🚨 Erro na rota /normalize:", err);
+    const errorMessage = err instanceof Error ? err.message : String(err);
+    return res.status(500).json({ error: "Erro interno no servidor.", details: errorMessage });
+  }
 });
 
 export default router;
