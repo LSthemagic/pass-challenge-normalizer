090d9831e3119c6748331490bb8492783dbafad9
Railan Santana <lansilvah14fsa@gmail.com>
2025-06-26 01:24:53 -0300

    feat: adicionar dicionários de mapeamento para tipos de acomodação e moedas

---
+++ src/providers/omnibees/dictionaries/board.dictionary.js
@@ -0,0 +1,10 @@
+/**
+ * Mapeia o CÓDIGO DA OMNIBEES para o nosso CÓDIGO PADRÃO.
+ * Chave: Código da Omnibees
+ * Valor: Código Padrão do nosso sistema (do room.json)
+ */
+export const omnibeesBoardDictionary = new Map([
+    [3, 'BB'],  // Bed & Breakfast
+    [10, 'FB'], // Full Board
+    [12, 'HB'], // Half Board
+]);
---
+++ src/providers/omnibees/dictionaries/currency.dictionary.js
@@ -0,0 +1,5 @@
+export const omnibeesCurrencyDictionary = new Map([
+    [16, 'BRL'],  // Bed & Breakfast
+    [9, 'USD'], // Full Board
+    [18, 'EUR'], // Half Board
+]);
