43103c22c7a3b628b425ac83b7f165b384fc697a
Railan Santana <lansilvah14fsa@gmail.com>
2025-06-26 01:25:20 -0300

    feat: tornar a função runNormalizationPipeline assíncrona e melhorar mensagens de log

--- src/strategies/orchestrator.js
+++ src/strategies/orchestrator.js
@@ -1,32 +1,24 @@
 import { getProviderAdapter } from "./01-adapter.strategy.js";
 import get from "lodash.get";
 
-export function runNormalizationPipeline(rawData, providerName) {
+export async function runNormalizationPipeline(rawData, providerName) {
   console.log(`[Orquestrador] Iniciando pipeline para o provedor: "${providerName}"`);
-
 
   const adapter = getProviderAdapter(providerName);
 
-
   if (!adapter || !adapter.config || !adapter.mapper) {
-    console.error(`[Orquestrador] ERRO: Adaptador, config ou mapper não foi encontrado para o provedor "${providerName}".`);
+    console.error(`[Orquestrador] ERRO: Adaptador, config ou mapper não foi encontrado para "${providerName}".`);
     throw new Error(`O provedor "${providerName}" não é suportado ou está mal configurado.`);
   }
-
-  console.log(`[Orquestrador] Estratégia 1: Usando mapeador manual para "${providerName}".`);
-
 
   const { config, mapper } = adapter;
   const { hotelArrayPath } = config;
 
   const hotelItems = get(rawData, hotelArrayPath, []);
-
-  console.log(`[Orquestrador] Mapeando ${hotelItems.length} hotéis.`);
+  console.log(`[Orquestrador] Mapeando ${hotelItems.length} hotéis brutos.`);
   
-  // Executa as funções de mapeamento manual
-  const finalHotels = mapper.hotel(hotelItems);
-
-  console.log(`[Orquestrador] Mapeamento manual concluído.`);
+  const finalHotels = await mapper.hotel(hotelItems);
+  console.log(`[Orquestrador] Mapeamento assíncrono concluído. ${finalHotels.length} hotéis normalizados.`);
   
-  return { hotel: finalHotels};
+  return { hotel: finalHotels };
 }
