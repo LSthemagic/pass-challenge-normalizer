c9c471106605295d31e6a178077a85fdbcaf30f7
Railan Santana <lansilvah14fsa@gmail.com>
2025-07-04 13:55:01 -0300

    refactor: remover comentários desnecessários do dicionário de códigos da Omnibees

--- src/integrations/hotelbeds/dictionaries/board.dictionary.js
+++ src/integrations/hotelbeds/dictionaries/board.dictionary.js
@@ -1,8 +1,3 @@
-/**
- * Mapeia o CÓDIGO DA OMNIBEES para o nosso CÓDIGO PADRÃO.
- * Chave: Código da Omnibees
- * Valor: Código Padrão do nosso sistema (do room.json)
- */
 export const omnibeesBoardDictionary = new Map([
     ['RO', 'RO'],
     ['SC', 'SC'],
